10eb45d659ba12395c77ba7efd2f50d93f19fa29
Bertrand Marchand <bertrand.marchand@lix.polytechnique.fr>
2024-03-22 17:28:04 -0400

    cutwidth

---
+++ cutwidth_public_exec.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+arr=()
+
+for i in {1..125}; 
+do
+    echo "------"
+    echo "running on file $i"
+    systemd-run --send-sighup --scope -p MemoryLimit=8000M timeout 1800 /usr/bin/time -f "exec time: %E (h:m:s)" ./target/release/cutwidth_algo cutwidth-public//$i.gr
+    exit_status=$?
+    if [[ $exit_status -eq 124 ]]; then
+        echo "TIME OUT"
+        arr+=('\033[0;31m#\033[0m]')
+    fi
+    if [[ $exit_status -eq 143 ]]; then
+        echo "OUT OF MEMORY"
+        arr+=('\033[0;31m#\033[0m]')
+    fi
+    if [[ $exit_status -eq 0 ]]; then
+        echo "SUCCESS"
+        arr+=('\032[0;31m#\033[0m]')
+    fi
+done
+
+for c in "${fruits[@]}"
+do
+    echo $c
+done
